58649f33fa41e386bc05e66fda29a6f1a5d64463
Moshu <k173634@nu.edu.pk>
2019-09-22 00:28:42 +0500

    page-2 template created for Photo Gallary!

--- packages/landing-gatsby/src/pages/page-2.js
+++ packages/landing-gatsby/src/pages/page-2.js
@@ -1,16 +1,51 @@
-import React from 'react';
 import { Link } from 'gatsby';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-);
+import React, { Fragment } from 'react';
+import Sticky from 'react-stickynode';
+import { Modal } from '@redq/reuse-modal';
+import { ThemeProvider } from 'styled-components';
+import { agencyTheme } from 'common/src/theme/agency';
+import { ResetCSS } from 'common/src/assets/css/style';
+import {
+  GlobalStyle,
+  AgencyWrapper,
+} from 'common/src/containers/Agency/agency.style';
+import Navbar from 'common/src/containers/Agency/Navbar';
+import Footer from 'common/src/containers/Agency/Footer';
+import { DrawerProvider } from 'common/src/contexts/DrawerContext';
+import '@redq/reuse-modal/es/index.css';
 
-export default SecondPage;
+export default () => {
+  return (
+    <ThemeProvider theme={agencyTheme}>
+      <Fragment>
+        <SEO title="Second_Page" />
+        <Modal />
+        <ResetCSS />
+        <GlobalStyle />
+        {/* End of agency head section */}
+        {/* Start agency wrapper section */}
+        <AgencyWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <Layout>
+            <SEO title="Page two" />
+            <br />
+            <br />
+            <br />
+            <h1>Hi from the second page</h1>
+            <p>Welcome to page 2</p>
+            <Link to="/">Go back to the homepage</Link>
+          </Layout>
+          <Footer />
+        </AgencyWrapper>
+        {/* End of agency wrapper section */}
+      </Fragment>
+    </ThemeProvider>
+  );
+};
