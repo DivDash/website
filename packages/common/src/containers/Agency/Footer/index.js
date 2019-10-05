1ab9f441f1b2ff9d62cf0d5317fde6e56c1120b9
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-10-05 11:05:16 +0500

    Corrected phone numbers

--- packages/common/src/containers/Agency/Footer/index.js
+++ packages/common/src/containers/Agency/Footer/index.js
@@ -19,7 +19,7 @@ const Footer = ({
   colTwo,
   titleStyle,
   logoStyle,
-  textStyle,
+  textStyle
 }) => {
   return (
     <FooterWrapper id="footerSection">
@@ -33,8 +33,14 @@ const Footer = ({
               logoStyle={logoStyle}
             />
             <Text content="Telic Solutions" {...textStyle} />
-            <Text content={<a href="tel:+92-312-9259476">+92-312-9259476</a>} {...textStyle} />
-            <Text content={<a href="tel:+1 (734) 474-0243">+1 (734) 474-0243</a>} {...textStyle} />
+            <Text
+              content={<a href="tel:+92-321-9259476">+92-321-9259476</a>}
+              {...textStyle}
+            />
+            <Text
+              content={<a href="tel:+1 (734) 474-0243">+1 (734) 474-0243</a>}
+              {...textStyle}
+            />
           </Box>
           {/* End of footer logo column */}
           <Box {...colTwo}>
@@ -68,7 +74,7 @@ Footer.propTypes = {
   colTwo: PropTypes.object,
   titleStyle: PropTypes.object,
   textStyle: PropTypes.object,
-  logoStyle: PropTypes.object,
+  logoStyle: PropTypes.object
 };
 
 // Footer default style
@@ -78,7 +84,7 @@ Footer.defaultProps = {
     flexBox: true,
     flexWrap: 'wrap',
     ml: '-4px',
-    mr: '-4px',
+    mr: '-4px'
   },
   // Footer col one style
   colOne: {
@@ -86,38 +92,38 @@ Footer.defaultProps = {
     mt: [0, '13px'],
     mb: ['30px', 0],
     pl: ['15px', 0],
-    pr: ['15px', '15px', 0],
+    pr: ['15px', '15px', 0]
   },
   // Footer col two style
   colTwo: {
     width: ['100%', '70%', '65%', '77%'],
     flexBox: true,
-    flexWrap: 'wrap',
+    flexWrap: 'wrap'
   },
   // Footer col default style
   col: {
     width: ['100%', '50%', '50%', '25%'],
     pl: '15px',
     pr: '15px',
-    mb: '30px',
+    mb: '30px'
   },
   // widget title default style
   titleStyle: {
     color: '#343d48',
     fontSize: '16px',
-    fontWeight: '700',
+    fontWeight: '700'
   },
   // Default logo size
   logoStyle: {
     width: '128px',
-    mb: '15px',
+    mb: '15px'
   },
   // widget text default style
   textStyle: {
     color: '#0f2137',
     fontSize: '16px',
-    mb: '10px',
-  },
+    mb: '10px'
+  }
 };
 
 export default Footer;
